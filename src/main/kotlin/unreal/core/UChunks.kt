a92aa7bd18d9e97245e2519190f2ec143dee6ffd
PizzaCrust <pizzacrustcoder@gmail.com>
2020-06-21 04:13:40 +0700

    branchdata and changelist

--- src/main/kotlin/unreal/core/UChunks.kt
+++ src/main/kotlin/unreal/core/UChunks.kt
@@ -2,6 +2,16 @@ package unreal.core
 
 import java.nio.ByteBuffer
 
+data class BranchData internal constructor(val raw: String, val major: Int, val minor: Int) {
+    companion object {
+        internal fun fromRawBranch(raw: String): BranchData {
+            val releaseVersion = raw.split("-")[1]
+            val versioning = releaseVersion.split("\\.".toRegex())
+            return BranchData(raw, versioning[0].toInt(), versioning[1].toInt())
+        }
+    }
+}
+
 class HeaderChunk internal constructor(): ByteSchema() {
     val networkMagic: Int by bytes(int32)
     val networkVersion: Int by staticBytes({ networkMagic == 0x2CF5A13D }, int32)
@@ -12,9 +22,14 @@ class HeaderChunk internal constructor(): ByteSchema() {
     val major: Short? by bytes({ println(id); networkVersion >= NetworkVersionHistory.HISTORY_SAVE_FULL_ENGINE_VERSION }, int16)
     val minor: Short? by bytes({ networkVersion >= NetworkVersionHistory.HISTORY_SAVE_FULL_ENGINE_VERSION }, int16)
     val patch: Short? by bytes({ networkVersion >= NetworkVersionHistory.HISTORY_SAVE_FULL_ENGINE_VERSION }, int16)
-    val changelist: Int? by bytes({ networkVersion >= NetworkVersionHistory.HISTORY_SAVE_FULL_ENGINE_VERSION }, int32)
-    val branch: String? by bytes({ networkVersion >= NetworkVersionHistory.HISTORY_SAVE_FULL_ENGINE_VERSION }, string)
-    val fallbackChangelist: Int? by bytes({ changelist == null }, int32)
+    private val changelist1: Int? by bytes({ networkVersion >= NetworkVersionHistory
+            .HISTORY_SAVE_FULL_ENGINE_VERSION }, int32)
+    val branch: BranchData? by bytes({ networkVersion >= NetworkVersionHistory.HISTORY_SAVE_FULL_ENGINE_VERSION }) {
+        BranchData.fromRawBranch(string)
+    }
+    private val changelist2: Int? by bytes({ changelist1 == null }, int32)
+    val changelist: Int?
+        get() = changelist1 ?: changelist2
     val levelNamesAndTimesSize: Int by bytes(int32)
     val skippedLevelData: Unit by bytes {
         slice(levelNamesAndTimesSize)
